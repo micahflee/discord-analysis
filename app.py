1e3c3858284f17a7217cd158edf65c2191cfd1ba
Micah Lee <micah@micahflee.com>
2017-08-31 10:04:15 -0700

    Add the beginnings of templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
