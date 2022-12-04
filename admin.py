e877cc2b02d4b2a8d1553a4fa484ca0476ac93d2
Micah Lee <micah@micahflee.com>
2022-12-04 14:04:15 -0800

    Format Python code with Black

--- admin.py
+++ admin.py
@@ -7,17 +7,20 @@ import glob
 import sqlalchemy
 from app import app, db, Server, User, Channel, Message
 
+
 def out(s):
     sys.stdout.write(s)
     sys.stdout.flush()
 
+
 def create_db():
-    db_path = app.config['SQLALCHEMY_DATABASE_URI'][10:] # strip "sqlite://"
+    db_path = app.config["SQLALCHEMY_DATABASE_URI"][10:]  # strip "sqlite://"
     if os.path.isfile(db_path):
         print("Database already exists")
     else:
         with app.app_context():
             db.create_all()
+
 
 def import_json(filename):
     print("Importing: {}".format(filename))
@@ -28,86 +31,107 @@ def import_json(filename):
 
     with app.app_context():
         # Add the servers
-        out('Adding servers: ')
-        for item in data['meta']['servers']:
-            name = item['name']
+        out("Adding servers: ")
+        for item in data["meta"]["servers"]:
+            name = item["name"]
 
             try:
                 server = Server(name)
                 db.session.add(server)
                 db.session.commit()
-                out('+')
+                out("+")
             except sqlalchemy.exc.IntegrityError:
                 db.session.rollback()
-                out('.')
-        out('\n')
+                out(".")
+        out("\n")
 
         # Add the users
-        out('Adding users: ')
-        for user_discord_id in data['meta']['users']:
-            name = data['meta']['users'][user_discord_id]['name']
+        out("Adding users: ")
+        for user_discord_id in data["meta"]["users"]:
+            name = data["meta"]["users"][user_discord_id]["name"]
 
             try:
                 user = User(user_discord_id, name)
                 db.session.add(user)
                 db.session.commit()
-                out('+')
+                out("+")
             except sqlalchemy.exc.IntegrityError:
                 db.session.rollback()
-                out('.')
-        out('\n')
+                out(".")
+        out("\n")
 
         # Add the channels
-        out('Adding channels: ')
-        for channel_discord_id in data['meta']['channels']:
-            name = data['meta']['channels'][channel_discord_id]['name']
-            server_id = data['meta']['channels'][channel_discord_id]['server']
-            server = Server.query.filter_by(name=data['meta']['servers'][server_id]['name']).first()
+        out("Adding channels: ")
+        for channel_discord_id in data["meta"]["channels"]:
+            name = data["meta"]["channels"][channel_discord_id]["name"]
+            server_id = data["meta"]["channels"][channel_discord_id]["server"]
+            server = Server.query.filter_by(
+                name=data["meta"]["servers"][server_id]["name"]
+            ).first()
 
             try:
                 channel = Channel(server, channel_discord_id, name)
                 db.session.add(channel)
                 db.session.commit()
-                out('+')
+                out("+")
             except sqlalchemy.exc.IntegrityError:
                 db.session.rollback()
-                out('.')
-        out('\n')
+                out(".")
+        out("\n")
 
         # Loop through each channel in data
         count = 0
-        for channel_discord_id in data['data']:
+        for channel_discord_id in data["data"]:
             # Get the channel
             channel = Channel.query.filter_by(discord_id=channel_discord_id).first()
 
             # Loop through each message in this channel
-            out('Adding messages from {}, #{}: '.format(channel.server.name, channel.name))
-            for discord_message_id in data['data'][channel_discord_id]:
+            out(
+                "Adding messages from {}, #{}: ".format(
+                    channel.server.name, channel.name
+                )
+            )
+            for discord_message_id in data["data"][channel_discord_id]:
                 try:
-                    timestamp = data['data'][channel_discord_id][discord_message_id]['t']
-                    message = data['data'][channel_discord_id][discord_message_id]['m']
+                    timestamp = data["data"][channel_discord_id][discord_message_id][
+                        "t"
+                    ]
+                    message = data["data"][channel_discord_id][discord_message_id]["m"]
 
-                    user_index = data['data'][channel_discord_id][discord_message_id]['u']
-                    discord_user_id = data['meta']['userindex'][user_index]
+                    user_index = data["data"][channel_discord_id][discord_message_id][
+                        "u"
+                    ]
+                    discord_user_id = data["meta"]["userindex"][user_index]
 
                     user = User.query.filter_by(discord_id=discord_user_id).first()
 
-                    if 'a' in data['data'][channel_discord_id][discord_message_id]:
-                        attachments_json = json.dumps(data['data'][channel_discord_id][discord_message_id]['a'])
+                    if "a" in data["data"][channel_discord_id][discord_message_id]:
+                        attachments_json = json.dumps(
+                            data["data"][channel_discord_id][discord_message_id]["a"]
+                        )
                     else:
                         attachments_json = None
 
-                    message = Message(channel.server, discord_message_id, timestamp, message, user, channel, attachments_json)
+                    message = Message(
+                        channel.server,
+                        discord_message_id,
+                        timestamp,
+                        message,
+                        user,
+                        channel,
+                        attachments_json,
+                    )
                     db.session.add(message)
                     db.session.commit()
-                    out('+')
+                    out("+")
                 except sqlalchemy.exc.IntegrityError:
                     db.session.rollback()
-                    out('.')
-            out('\n')
+                    out(".")
+            out("\n")
 
     print("Import complete")
     print("")
+
 
 def user_stats():
     users = User.query.order_by(User.name).all()
@@ -118,7 +142,9 @@ def user_stats():
     for user in users:
         print("User: {}".format(user.name))
         for server in servers:
-            message_count = Message.query.filter_by(server=server).filter_by(user=user).count()
+            message_count = (
+                Message.query.filter_by(server=server).filter_by(user=user).count()
+            )
             if message_count > 0:
                 if server.name in server_users:
                     server_users[server.name] += 1
@@ -133,27 +159,28 @@ def user_stats():
     for server_name in server_users:
         print("{}: {} users".format(server_name, server_users[server_name]))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='subcommand')
-    subparsers.add_parser('create-db')
-    parser_import_json = subparsers.add_parser('import-json')
-    parser_import_json.add_argument('filename')
-    subparsers.add_parser('user-stats')
+    subparsers = parser.add_subparsers(dest="subcommand")
+    subparsers.add_parser("create-db")
+    parser_import_json = subparsers.add_parser("import-json")
+    parser_import_json.add_argument("filename")
+    subparsers.add_parser("user-stats")
     args = parser.parse_args()
 
     cmd = args.subcommand
 
-    if cmd == 'create-db':
+    if cmd == "create-db":
         create_db()
 
-    elif cmd == 'import-json':
-        filenames = glob.glob(args.filename.replace('~', os.environ['HOME']))
+    elif cmd == "import-json":
+        filenames = glob.glob(args.filename.replace("~", os.environ["HOME"]))
         for filename in filenames:
             import_json(filename)
 
-    elif cmd == 'user-stats':
+    elif cmd == "user-stats":
         user_stats()
 
     else:
--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from flask import Flask, render_template, request, escape, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('app.cfg')
+app.config.from_pyfile("app.cfg")
 
 # Hack to support mysql and unicode
-use_mysql = app.config['SQLALCHEMY_DATABASE_URI'].startswith('mysql')
+use_mysql = app.config["SQLALCHEMY_DATABASE_URI"].startswith("mysql")
 if use_mysql:
     from sqlalchemy.dialects.mysql import VARCHAR, TEXT
 
@@ -17,15 +17,20 @@ db = SQLAlchemy(app)
 
 # Get the page and per_page args from query string, as ints
 def get_pagination_args():
-    page = request.args.get('page', 1)
-    per_page = request.args.get('per_page', 2000)
+    page = request.args.get("page", 1)
+    per_page = request.args.get("per_page", 2000)
     return (int(page), int(per_page))
+
 
 # A discord server
 class Server(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     if use_mysql:
-        name = db.Column(VARCHAR(128, charset='utf8mb4', collation='utf8mb4_unicode_ci'), unique=True, nullable=False)
+        name = db.Column(
+            VARCHAR(128, charset="utf8mb4", collation="utf8mb4_unicode_ci"),
+            unique=True,
+            nullable=False,
+        )
     else:
         name = db.Column(db.String(128), unique=True, nullable=False)
 
@@ -35,13 +40,16 @@ class Server(db.Model):
     def __init__(self, name):
         self.name = name
 
+
 # A user in a chat room team
 class User(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     discord_id = db.Column(db.String(128), unique=True, nullable=False)
 
     if use_mysql:
-        name = db.Column(VARCHAR(128, charset='utf8mb4', collation='utf8mb4_unicode_ci'))
+        name = db.Column(
+            VARCHAR(128, charset="utf8mb4", collation="utf8mb4_unicode_ci")
+        )
     else:
         name = db.Column(db.String(128))
 
@@ -52,23 +60,26 @@ class User(db.Model):
         self.name = name
 
     def permalink(self):
-        return '/user/{}'.format(self.id)
+        return "/user/{}".format(self.id)
 
     def message_count(self):
         return Message.query.filter_by(user=self).count()
+
 
 # A channel
 class Channel(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     discord_id = db.Column(db.String(128), unique=True, nullable=False)
     if use_mysql:
-        name = db.Column(VARCHAR(128, charset='utf8mb4', collation='utf8mb4_unicode_ci'))
+        name = db.Column(
+            VARCHAR(128, charset="utf8mb4", collation="utf8mb4_unicode_ci")
+        )
     else:
         name = db.Column(db.String(128))
 
     messages = db.relationship("Message", back_populates="channel")
 
-    server_id = db.Column(db.Integer, db.ForeignKey('server.id'))
+    server_id = db.Column(db.Integer, db.ForeignKey("server.id"))
     server = db.relationship("Server", back_populates="channels")
 
     def __init__(self, server, discord_id, name):
@@ -77,10 +88,11 @@ class Channel(db.Model):
         self.name = name
 
     def permalink(self):
-        return '/channel/{}'.format(self.id)
+        return "/channel/{}".format(self.id)
 
     def message_count(self):
         return Message.query.filter_by(channel=self).count()
+
 
 # A message posted in a channel
 class Message(db.Model):
@@ -88,22 +100,35 @@ class Message(db.Model):
     discord_id = db.Column(db.String(128), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime)
     if use_mysql:
-        message = db.Column(VARCHAR(4096, charset='utf8mb4', collation='utf8mb4_unicode_ci'))
-        attachments_json = db.Column(VARCHAR(4096, charset='utf8mb4', collation='utf8mb4_unicode_ci'))
+        message = db.Column(
+            VARCHAR(4096, charset="utf8mb4", collation="utf8mb4_unicode_ci")
+        )
+        attachments_json = db.Column(
+            VARCHAR(4096, charset="utf8mb4", collation="utf8mb4_unicode_ci")
+        )
     else:
         message = db.Column(db.String(4096))
         attachments_json = db.Column(db.String(4096))
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     user = db.relationship("User", back_populates="messages")
 
-    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'))
+    channel_id = db.Column(db.Integer, db.ForeignKey("channel.id"))
     channel = db.relationship("Channel", back_populates="messages")
 
-    server_id = db.Column(db.Integer, db.ForeignKey('server.id'))
+    server_id = db.Column(db.Integer, db.ForeignKey("server.id"))
     server = db.relationship("Server", back_populates="messages")
 
-    def __init__(self, server, discord_id, timestamp, message, user, channel, attachments_json=None):
+    def __init__(
+        self,
+        server,
+        discord_id,
+        timestamp,
+        message,
+        user,
+        channel,
+        attachments_json=None,
+    ):
         self.server = server
         self.discord_id = discord_id
         self.timestamp = datetime.datetime.fromtimestamp(timestamp / 1000)
@@ -114,27 +139,29 @@ class Message(db.Model):
             self.attachments_json = attachments_json
 
     def formatted_timestamp(self):
-        return self.timestamp.strftime('%b %d, %Y %I:%M:%S %p')
+        return self.timestamp.strftime("%b %d, %Y %I:%M:%S %p")
 
     def permalink(self):
-        return '/view/{}'.format(self.id)
+        return "/view/{}".format(self.id)
 
     def highlight(self, query):
         # Make sure to escape the message here, and replace newslines with line breaks
-        m = str(escape(self.message)).replace('\n', '<br>\n')
+        m = str(escape(self.message)).replace("\n", "<br>\n")
 
         # If there isn't a query, return the original message
         if not query:
             return m
 
-        new_m = ''
+        new_m = ""
         index = 0
         while True:
             new_index = m.lower().find(query.lower(), index)
             if new_index > 0:
                 # Found
                 new_m += m[index:new_index]
-                new_m += "<span class='highlight'>{}</span>".format(m[new_index:new_index+len(query)])
+                new_m += "<span class='highlight'>{}</span>".format(
+                    m[new_index : new_index + len(query)]
+                )
                 index = new_index + len(query)
             else:
                 # Not found
@@ -149,18 +176,21 @@ class Message(db.Model):
 
         return json.loads(self.attachments_json)
 
+
 data = None
 
-@app.route('/')
+
+@app.route("/")
 def index():
     servers = Server.query.all()
-    return render_template('index.html', servers=servers)
+    return render_template("index.html", servers=servers)
 
-@app.route('/search')
+
+@app.route("/search")
 def search():
-    q = request.args.get('q')
-    s = request.args.get('s', 0)
-    if s == '':
+    q = request.args.get("q")
+    s = request.args.get("s", 0)
+    if s == "":
         s = 0
     page, per_page = get_pagination_args()
 
@@ -169,87 +199,151 @@ def search():
     messages = Message.query
     if server:
         messages = messages.filter_by(server=server)
-    pagination = messages.filter(Message.message.like("%{}%".format(q))).order_by(Message.timestamp).paginate(page, per_page, False)
+    pagination = (
+        messages.filter(Message.message.like("%{}%".format(q)))
+        .order_by(Message.timestamp)
+        .paginate(page, per_page, False)
+    )
 
     if server:
-        description = 'Search {}: {}'.format(server.name, q)
+        description = "Search {}: {}".format(server.name, q)
     else:
-        description = 'Search: {}'.format(q)
+        description = "Search: {}".format(q)
 
     servers = Server.query.all()
-    pagination_link = '/search?q={}&s={}'.format(q, s)
-    return render_template('results.html', q=q, s=int(s), servers=servers, pagination=pagination, pagination_link=pagination_link, description=description)
+    pagination_link = "/search?q={}&s={}".format(q, s)
+    return render_template(
+        "results.html",
+        q=q,
+        s=int(s),
+        servers=servers,
+        pagination=pagination,
+        pagination_link=pagination_link,
+        description=description,
+    )
 
-@app.route('/view/<int:message_id>')
+
+@app.route("/view/<int:message_id>")
 def view(message_id):
-    q = request.args.get('q')
+    q = request.args.get("q")
 
     # Look up the Message
     message = Message.query.filter_by(id=message_id).first()
     if not message:
-        flash('Invalid message')
-        return redirect('/')
+        flash("Invalid message")
+        return redirect("/")
 
     # Find messages before and after this one
-    prev_messages = Message.query.filter_by(channel=message.channel).filter(Message.timestamp < message.timestamp).order_by(Message.timestamp.desc()).limit(20).all()
+    prev_messages = (
+        Message.query.filter_by(channel=message.channel)
+        .filter(Message.timestamp < message.timestamp)
+        .order_by(Message.timestamp.desc())
+        .limit(20)
+        .all()
+    )
     prev_messages.reverse()
-    next_messages = Message.query.filter_by(channel=message.channel).filter(Message.timestamp > message.timestamp).order_by(Message.timestamp).limit(20).all()
+    next_messages = (
+        Message.query.filter_by(channel=message.channel)
+        .filter(Message.timestamp > message.timestamp)
+        .order_by(Message.timestamp)
+        .limit(20)
+        .all()
+    )
 
     # Create a description
-    description = 'Message by {}, in {}, #{}'.format(message.user.name, message.server.name, message.channel.name)
+    description = "Message by {}, in {}, #{}".format(
+        message.user.name, message.server.name, message.channel.name
+    )
 
     servers = Server.query.all()
-    return render_template('view.html', q=q, s=message.server.id, channel=message.channel, servers=servers, description=description, active_message_id=message.id, message=message, prev_messages=prev_messages, next_messages=next_messages)
+    return render_template(
+        "view.html",
+        q=q,
+        s=message.server.id,
+        channel=message.channel,
+        servers=servers,
+        description=description,
+        active_message_id=message.id,
+        message=message,
+        prev_messages=prev_messages,
+        next_messages=next_messages,
+    )
 
-@app.route('/channel/<int:channel_id>')
+
+@app.route("/channel/<int:channel_id>")
 def channel(channel_id):
     page, per_page = get_pagination_args()
 
     # Look up the Channel
     channel = Channel.query.filter_by(id=channel_id).first()
     if not channel:
-        flash('Invalid channel')
-        return redirect('/')
+        flash("Invalid channel")
+        return redirect("/")
 
     # Look up messages
-    pagination = Message.query.filter_by(channel=channel).order_by(Message.timestamp).paginate(page, per_page, False)
+    pagination = (
+        Message.query.filter_by(channel=channel)
+        .order_by(Message.timestamp)
+        .paginate(page, per_page, False)
+    )
 
     # Description
-    description = 'Messages in {}, #{}'.format(channel.server.name, channel.name)
+    description = "Messages in {}, #{}".format(channel.server.name, channel.name)
 
     servers = Server.query.all()
-    pagination_link = '/channel/{}?'.format(channel_id)
-    return render_template('results.html', s=channel.server.id, channel=channel, servers=servers, pagination=pagination, pagination_link=pagination_link, description=description)
+    pagination_link = "/channel/{}?".format(channel_id)
+    return render_template(
+        "results.html",
+        s=channel.server.id,
+        channel=channel,
+        servers=servers,
+        pagination=pagination,
+        pagination_link=pagination_link,
+        description=description,
+    )
 
-@app.route('/users')
+
+@app.route("/users")
 def user_list():
     users = User.query.all()
     servers = Server.query.all()
-    return render_template('user_list.html', servers=servers, users=users)
+    return render_template("user_list.html", servers=servers, users=users)
 
-@app.route('/user/<int:user_id>')
+
+@app.route("/user/<int:user_id>")
 def user(user_id):
     page, per_page = get_pagination_args()
 
     # Look up the User
     user = User.query.filter_by(id=user_id).first()
     if not user:
-        flash('Invalid user')
-        return redirect('/')
+        flash("Invalid user")
+        return redirect("/")
 
     # Look up messages
-    pagination = Message.query.filter_by(user=user).order_by(Message.timestamp).paginate(page, per_page, False)
+    pagination = (
+        Message.query.filter_by(user=user)
+        .order_by(Message.timestamp)
+        .paginate(page, per_page, False)
+    )
 
     # Description
-    description = 'Messages from @{}'.format(user.name)
+    description = "Messages from @{}".format(user.name)
 
     servers = Server.query.all()
-    pagination_link = '/user/{}?'.format(user_id)
-    return render_template('results.html', servers=servers, pagination=pagination, pagination_link=pagination_link, description=description)
+    pagination_link = "/user/{}?".format(user_id)
+    return render_template(
+        "results.html",
+        servers=servers,
+        pagination=pagination,
+        pagination_link=pagination_link,
+        description=description,
+    )
 
 
 def main():
     app.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
