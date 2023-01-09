f4f4a48f37e70ec0b1b32881c26d9f4d31b28272
Micah Lee <micah@micahflee.com>
2023-01-08 18:48:54 -0800

    Switch discord-json-search.py to use Click

--- discord-json-search.py
+++ discord-json-search.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
 import sys
-import argparse
 import json
 import datetime
+import click
 
-def colored(msg, color=None, bold=False, underline=False):
-    c = {
-        'purple': '\033[95m',
-        'blue': '\033[94m',
-        'green': '\033[92m',
-        'yellow': '\033[93m',
-        'red': '\033[91m',
-        'gray': '\033[90m',
-        'end': '\033[0m',
-        'bold': '\033[1m',
-        'underline': '\033[4m'
-    }
-    s = ''
-    if bold:
-        s += c['bold']
-    if underline:
-        s += c['underline']
-    if color:
-        s += c[color]
-    s += msg
-    s += c['end']
-    return s
 
 def highlight(message, query):
-    new_message = ''
+    new_message = ""
     index = 0
     while True:
         new_index = message.lower().find(query.lower(), index)
         if new_index > 0:
             # Found
             new_message += message[index:new_index]
-            new_message += colored(message[new_index:new_index+len(query)], underline=True)
+            new_message += click.style(
+                message[new_index : new_index + len(query)], underline=True
+            )
             index = new_index + len(query)
         else:
             # Not found
@@ -44,52 +24,63 @@ def highlight(message, query):
 
     return new_message
 
+
 def display(channel_name, server_name, user_name, timestamp, message, query):
-    print('{} {}'.format(colored('#{}'.format(channel_name), 'purple'), colored('[server: {}]'.format(server_name), 'gray')))
-    print('{} {}'.format(colored(user_name, bold=True), colored(timestamp.strftime('%c'), 'gray')))
-    print(highlight(message, query))
-    print('')
+    click.echo(
+        "{} {}".format(
+            click.style("#{}".format(channel_name), fg="bright_magenta"),
+            click.style("[server: {}]".format(server_name), fg="bright_black"),
+        )
+    )
+    click.echo(
+        "{} {}".format(
+            click.style(user_name, bold=True),
+            click.style(timestamp.strftime("%c"), fg="bright_black"),
+        )
+    )
+    click.echo(highlight(message, query))
+    click.echo("")
+
 
 def search(data, query):
     # Loop through each channel
-    for channel_id in data['data']:
+    for channel_id in data["data"]:
         # Get the channel name and server name
-        channel_name = data['meta']['channels'][channel_id]['name']
-        server_name = data['meta']['servers'][data['meta']['channels'][channel_id]['server']]['name']
+        channel_name = data["meta"]["channels"][channel_id]["name"]
+        server_name = data["meta"]["servers"][
+            data["meta"]["channels"][channel_id]["server"]
+        ]["name"]
 
-        for message_id in data['data'][channel_id]:
+        for message_id in data["data"][channel_id]:
             # Pull the user data, timestamp, and message body from the message
-            user_index = data['data'][channel_id][message_id]['u']
-            user_id = data['meta']['userindex'][user_index]
-            user_name = data['meta']['users'][user_id]['name']
-            timestamp = datetime.datetime.fromtimestamp(data['data'][channel_id][message_id]['t'] / 1000)
-            message = data['data'][channel_id][message_id]['m']
+            user_index = data["data"][channel_id][message_id]["u"]
+            user_id = data["meta"]["userindex"][user_index]
+            user_name = data["meta"]["users"][user_id]["name"]
+            timestamp = datetime.datetime.fromtimestamp(
+                data["data"][channel_id][message_id]["t"] / 1000
+            )
+            message = data["data"][channel_id][message_id]["m"]
 
             # Is the query in the message?
             if query.lower() in message.lower():
                 display(channel_name, server_name, user_name, timestamp, message, query)
 
-def main():
-    # Arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename", help="Discord JSON filename")
-    parser.add_argument("query", help="Search query")
-    parser.parse_args()
-    args = parser.parse_args()
 
-    filename = args.filename
-    query = args.query
-
-    # Load the json file
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+@click.argument("query")
+def main(filename, query):
+    # Load the JSON file
     try:
         with open(filename) as data_file:
             data = json.load(data_file)
     except:
-        print('Failed to load JSON file')
+        print("Failed to load JSON file")
         sys.exit()
 
     # Search
     search(data, query)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
